5060f6838b45702358bf8a24c903e3b5e0588d22
HenKroh <141871569+HenKroh@users.noreply.github.com>
2025-02-11 15:33:47 -0300

    Create io.asm

---
+++ submission/io.asm
@@ -0,0 +1,37 @@
+        @  /0000
+        SC /0200
+        HM /0000  
+
+        @  /0200
+        JP /0000
+        GD /0000
+        SB TEMP
+        MM X
+        GD /0000
+        GD /0000
+        SB TEMP
+        AD X
+        MM X
+        DV HEX
+        ML HEX
+        MM Y
+        LD X
+        SB Y
+        SUB DEZ
+        JN NEG
+        LD X
+        SUB DEZ
+        AD VAI
+        JP COM
+NEG     LD X
+COM     AD TEMP
+        PD /0100
+        RS /0200
+
+        @  /0100
+X       K  /0000
+Y       K  /0000
+DEZ     K  /000A
+TEMP    K  /3030
+HEX     K  /0010
+VAI     K  /0100
